0fa904a83ae32324af1b4e30d920306df7dffd2b
joe1-code <john4m2@gmail.com>
2023-05-24 16:10:45 +0300

    added account lockout code

--- Home/Login/login.php
+++ Home/Login/login.php
@@ -1,5 +1,6 @@
 <?php
 include '../../Config/connect.php';
+session_start();
 
 $username = $_REQUEST['username'];
 $pass = $_REQUEST['password'];
@@ -12,10 +13,25 @@ $db = $con->query($sql);
 if($db->num_rows>0){
  while($row=$db->fetch_assoc()){
  if(!password_verify($pass, $row['password'])){
-  echo "not a user";
+  if(isset($_SESSION['login_attempts'])){
+   $_SESSION['login_attempts']++;
+  }
+  else{
+   $_SESSION['login_attempts'] = 1;
+  }
+  if($_SESSION['login_attempts']>=3){
+   echo "you are locked out.Please try again later";
+  
+  }
+  else{
+   echo "wrong credentials. Please try again later";
+   
+  }
  }
  else{
+  $_SESSION['logged_in'] = TRUE;
   header("location:../addition/function.php");
+  return true;
  }
 
 
--- Home/Register/register.php
+++ Home/Register/register.php
@@ -9,14 +9,22 @@ $occupation = $_REQUEST['occupation'];
 $email = $_REQUEST['email'];
 $password = $_REQUEST['password'];
 
+#preventation of sql injection
+$sanitizedFname = $con->real_escape_string($firstname);
+$sanitizedLname = $con->real_escape_string($lastname);
+$sanitizedDOB = $con -> real_escape_string($dob);
+$sanitizedOffice = $con->real_escape_string($office);
+$sanitizedOccupation = $con->real_escape_string($occupation);
+$sanitizedEmail = $con -> real_escape_string($email);
+$sanitizedPass = $con->real_escape_string($password);
 #hash the password for security
-$pass = password_hash($password, PASSWORD_DEFAULT);
+$pass = password_hash($sanitizedPass, PASSWORD_DEFAULT);
 
 #create the unique ID for the user
 $userID = uniqid();
 
 $sql = "INSERT INTO workersfund(fname, lname, DOB, office, occupation, email, password, uid)
-                          VALUES('$firstname', '$lastname', '$dob', '$office', '$occupation', '$email', '$pass', '$userID')";
+                          VALUES('$sanitizedFname', '$sanitizedLname', '$sanitizedDOB', '$sanitizedOffice', '$sanitizedOccupation', '$sanitizedEmail', '$pass', '$userID')";
 
 if(mysqli_query($con, $sql)){
  header("location:../Login/login.html");
